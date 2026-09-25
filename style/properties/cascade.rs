054e4d1ce85bc6274e0bab3d1c2991ee04eebe4c
Jacques Germishuys <jacques.germishuys@bloudraad.io>
2026-09-25 16:07:32 +0100

    fix(cascade): preserve author background provenance from rule cache

--- style/properties/cascade.rs
+++ style/properties/cascade.rs
@@ -1489,6 +1489,7 @@ impl<'b> Cascade<'b> {
         // would as well.  It matches the same rules, so it is the right thing
         // to do anyways, even if it's only used on inherited properties.
         let bits_to_copy = ComputedValueFlags::HAS_AUTHOR_SPECIFIED_BORDER_BACKGROUND
+            | ComputedValueFlags::HAS_AUTHOR_SPECIFIED_BACKGROUND_COLOR
             | ComputedValueFlags::HAS_AUTHOR_SPECIFIED_BD_FLOAT_DISPLACE
             | ComputedValueFlags::DEPENDS_ON_SELF_FONT_METRICS
             | ComputedValueFlags::DEPENDS_ON_INHERITED_FONT_METRICS
